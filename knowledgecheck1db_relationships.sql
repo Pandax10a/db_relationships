29e0b3fc59533063370273362acd92be3d5d500f
panda1 <blueblue0011@gmail.com>
2022-09-13 18:06:34 -0700

    done

---
+++ knowledgecheck1db_relationships.sql
@@ -0,0 +1,6 @@
+insert into citizen(name, address, phone_number, date_of_birth)
+values('one', '117 mcdonald ave, somewhere bc, ca', '555-555-5555', '2000-01-01'), ('two', '103 somehwhere ave, somewhere bc, ca', '555-555-5555', '2001-01-01');
+
+insert into tax_account(date_created, account_number)
+values('2018-01-01', '1234www3311');
+
